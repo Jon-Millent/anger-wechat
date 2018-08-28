76a3966bb2e969ceb957f96f439f9ff22b7e5ba0
奇怪的猫 <millent@126.com>
2018-08-28 10:53:52 +0800

    Update initwx.js

--- test/web/initwx.js
+++ test/web/initwx.js
@@ -1,9 +1,9 @@
 var AngerWechat = require('../../index')
 
 var weixinApi = new AngerWechat({
-  appId: 'wxf638c1f64239e786',
-  appSecret: 'e10c0db08562640e0a9fffab940724c2',
-  authUrl: 'http://www.toolos.cc/get-weixin-code.html'
+  appId: '',
+  appSecret: '',
+  authUrl: 'http://www.test.cc/get-weixin-code.html'
 })
 
 
